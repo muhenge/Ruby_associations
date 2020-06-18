6908f4a30923773f0c663d7e60b106a7d5be54c6
muhenge <mugunga5@gmail.com>
2020-06-19 01:51:20 +0200

    Set up rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   has_many :events, foreign_key: 'creator_id', class_name: 'Event'
   has_many :event_attending
   has_many :attend, through: :event_attending, source: :event
-  def self.past_event
-    where('date < ?', Date.today)
-  end
+  # def self.past_event
+  #   where('date < ?', Date.today)
+  # end
 
-  def self.upcoming_event
-    where('date >= ?', Date.today)
-  end
+  # def self.upcoming_event
+  #   where('date >= ?', Date.today)
+  # end
 
   scope :previously, -> { where('date < ?', Date.today) }
   scope :upcoming, -> { where('date >= ?', Date.today) }
