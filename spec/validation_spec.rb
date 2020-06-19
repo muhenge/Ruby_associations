5e9661ec7a33bb01ebce21f355c107ccb28ea4f9
muhenge <mugunga5@gmail.com>
2020-06-19 20:59:49 +0200

    Add event validations test

---
+++ spec/validation_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+
+RSpec.describe Event, type: :model do
+    context 'Event description is always present' do
+        it 'is invalid without description input' do
+            event = Event.new(description: nil)
+            event.valid?
+            expect(event.errors[:description]).to include("can't be blank")
+        end
+    end
+    context 'Event date is always present' do
+        it 'is invalid without date input' do
+            event = Event.new(description: nil)
+            event.valid?
+            expect(event.errors[:description]).to include("can't be blank")
+        end
+    end
+
+end
