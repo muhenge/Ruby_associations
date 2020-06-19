66878671b429dee05e89acc4c9824078b526f9e1
muhenge <mugunga5@gmail.com>
2020-06-19 02:02:30 +0200

    Add username type test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,9 @@
+require 'rails_helper'
 
+RSpec.describe User, type: :model do
+  it 'should create a string' do
+    user = User.new(username: 'herve')
+    user.save
+    expect(user.username).to be_an_instance_of(String)
+  end
+end
