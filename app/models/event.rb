35981c630ccb69a842cda4ce16acf6b9aa5139fb
muhenge <mugunga5@gmail.com>
2020-06-19 17:42:33 +0200

    Add Authentification test for user model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,13 +3,13 @@ class Event < ApplicationRecord
   has_many :event_attending
   has_many :attendees, through: :event_attending, source: :user
 
-  def self.past_event
-    where('date < ?', Date.today)
-  end
+  # def self.past_event
+  #   where('date < ?', Date.today)
+  # end
 
-  def self.upcoming_event
-    where('date >= ?', Date.today)
-  end
+  # def self.upcoming_event
+  #   where('date >= ?', Date.today)
+  # end
 
   scope :previously, -> { where('date < ?', Date.today) }
   scope :upcoming, -> { where('date >= ?', Date.today) }
