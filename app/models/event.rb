a8952874472dcb9f1e4828bb73a37d3a6f601a05
rindra josia <rrindrajosia@gmail.com>
2020-06-19 20:29:48 +0300

    add validation for user and event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  validates :description, presence: true
+  validates :date, presence: true
   belongs_to :creator, class_name: 'User'
   has_many :event_attending
   has_many :attendees, through: :event_attending, source: :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
   has_many :events, foreign_key: 'creator_id', class_name: 'Event'
   has_many :event_attending
   has_many :attend, through: :event_attending, source: :event
-  # def self.past_event
-  #   where('date < ?', Date.today)
-  # end
-
-  # def self.upcoming_event
-  #   where('date >= ?', Date.today)
-  # end
-
-  scope :previously, -> { where('date < ?', Date.today) }
-  scope :upcoming, -> { where('date >= ?', Date.today) }
 end
